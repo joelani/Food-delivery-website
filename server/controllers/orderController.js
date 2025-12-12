9a0927a080068c5cc33e9f3bc651a3350b8b743c
joe <josepholaniyi820@gmail.com>
2025-12-12 06:59:42 +0100

    Implement order placement functionality with Stripe integration; add order model and routes

---
+++ server/controllers/orderController.js
@@ -0,0 +1,67 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+import dotenv from "dotenv";
+dotenv.config();
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+//Placing order from frontend.
+const placeOrder = async (req, res) => {
+  console.log("Stripe Key:", process.env.STRIPE_SECRET_KEY);
+  const frontend_url = "http://localhost:5173";
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+    });
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, {
+      cartData: {},
+    });
+
+    const lineItems = req.body.items.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    lineItems.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: 2 * 100,
+      },
+      quantity: 1,
+    });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Order placed successfully",
+      session_url: session.url,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Error placing order", error });
+    console.log("Error placing order:", error);
+  }
+};
+
+export { placeOrder };
---
+++ server/routes/orderRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import authMiddleware from "../middlewares/auth.js";
+import { placeOrder } from "../controllers/orderController.js";
+
+const orderRouter = express.Router();
+orderRouter.post("/placeorder", authMiddleware, placeOrder);
+
+export default orderRouter;
