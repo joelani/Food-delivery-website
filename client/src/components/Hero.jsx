8ef134bdc3fac77588d26a938c998beea869b365
joe <josepholaniyi820@gmail.com>
2025-12-13 21:14:47 +0100

    updated

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="w-full max-w-[1440px] mx-auto">
       <div className=" w-full bg-[url('/hero_imgg.png')] min-h-[450px] p-9 bg-cover bg-center bg-no-repeat rounded-2xl relative flex flex-col md:flex-row items-center justify-between px-6 md:px-12 lg:px-16 py-8 md:py-12 lg:py-16 gap-6 md:gap-0 ">
         <div className="absolute inset-0 bg-linear-to-r from-black/30 via-black/15 to-transparent rounded-2xl"></div>
-        {/* Hero Content */}
+        {/* Hero Contentt */}
 
         <div className="text-center md:text-left max-w-lg absolute bottom-5 ">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold  text-BackgroundLight mb-4">
